0870d876ec820bf4bc95a339dcb9ebc0426e5a71
Shiying Chen <chenshiying2002@gmail.com>
2026-07-04 19:11:06 +1000

    Initial Commit

---
+++ Slime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slime
+//
+//  Created by shiying on 2026/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
